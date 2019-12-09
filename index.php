22a68de4e43bdacfbf7b816a3e2abf8dd5e53a2a
guilhermecolombo <lordshockgui999@gmail.com>
2019-12-09 16:28:04 -0200

    Segunda Versao projeto

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang = "pt-BR">
+	
+	<head>
+		
+		<title>Futebol 3</title>
+		<meta charset="utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1" />
+		
+		<link rel = "stylesheet" href = "css/bootstrap.min.css" />
+		
+		<script src = "js/jquery-3.4.1.min.js"></script>
+		<script src = "js/bootstrap.min.js"></script>		
+	</head>
+	<?php
+	include("cabecalho.php");
+	?>
+	<body>
+		
+	</body>
+	
+</html>
